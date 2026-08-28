b7c441db70e779734399cabaa9003acd0dfce173
Nordin Suleimani <nordin.suleimani@gmail.com>
2026-08-28 09:54:42 +0200

    fix: Removed double connection function

--- apps/time/time_queries.py
+++ apps/time/time_queries.py
@@ -2,30 +2,24 @@ from shared.credentials import get_database_credentials
 import psycopg2
 from datetime import datetime
 
-# helper function to set up connection 
-def get_connection() -> psycopg2.extensions.connection:
 
+# helper function to set up connection
+def get_connection() -> psycopg2.extensions.connection:
 
     host, database, user, password, port = get_database_credentials()
 
-   
-    connection =  psycopg2.connect(
+    connection = psycopg2.connect(
         host=host,
         dbname=database,
         user=user,
         password=password,
         port=port,
-        sslmode="require"
+        sslmode="require",
     )
 
-    return connection    
+    return connection
 
-def get_connection():
-    host, database, user, password, port = get_database_credentials()
-    return psycopg2.connect(
-        host=host, dbname=database, user=user,
-        password=password, port=port, sslmode="require"
-    )
+
 # add consultant - returnes id
 def add_consultant(name: str, email: str) -> int:
 
@@ -35,17 +29,17 @@ def add_consultant(name: str, email: str) -> int:
             with con.cursor() as cursor:
                 # execute query
                 cursor.execute(
-                    '''
+                    """
                     INSERT INTO consultants (name, email) 
                     VALUES (%s, %s) RETURNING id
-                    ''',
+                    """,
                     (name, email),
                 )
-                 # fetch & return the new id
+                # fetch & return the new id
                 consultant_id = cursor.fetchone()[0]
-                return consultant_id  
+                return consultant_id
     except (Exception, psycopg2.DatabaseError) as e:
-        print('Database error: ', e)
+        print("Database error: ", e)
 
 
 # add customer - returnes id
@@ -57,34 +51,34 @@ def add_customer(name: str) -> int:
             with con.cursor() as cursor:
                 # execute query
                 cursor.execute(
-                    '''
+                    """
                     INSERT INTO customers (name)
                     VALUES (%s) RETURNING id
-                    ''',
-                    (name, ),
+                    """,
+                    (name,),
                 )
                 # fetch & return the new id
                 customer_id = cursor.fetchone()[0]
                 return customer_id
     except (Exception, psycopg2.DatabaseError) as e:
-        print('Database error: ', e)
+        print("Database error: ", e)
 
 
-# find consultant by email - return the id 
+# find consultant by email - return the id
 def find_consultant(email: str) -> int | None:
-     
+
     try:
         # connect to database
         with get_connection() as con:
             with con.cursor() as cursor:
                 # execute query
                 cursor.execute(
-                    '''
+                    """
                     SELECT id
                     FROM consultants
                     WHERE email = %s
-                    ''',
-                    (email, ),
+                    """,
+                    (email,),
                 )
                 # fetch id - return id or None
                 result = cursor.fetchone()
@@ -93,7 +87,7 @@ def find_consultant(email: str) -> int | None:
                 else:
                     return result[0]
     except (Exception, psycopg2.DatabaseError) as e:
-        print('Database error: ', e)
+        print("Database error: ", e)
 
 
 # find customer return id
@@ -104,12 +98,12 @@ def find_customer(name: str) -> int | None:
             with con.cursor() as cursor:
                 # execute query
                 cursor.execute(
-                    '''
+                    """
                     SELECT id
                     FROM customers
                     WHERE name = %s
-                    ''',
-                    (name, ),
+                    """,
+                    (name,),
                 )
                 # fetch id - return id / None
                 result = cursor.fetchone()
@@ -118,13 +112,16 @@ def find_customer(name: str) -> int | None:
                 else:
                     return result[0]
     except (Exception, psycopg2.DatabaseError) as e:
-        print('Database error: ', e)
+        print("Database error: ", e)
 
 
 # add time entry
 def add_time_entry(
-        consultant_id: int, customer_id: int,
-        start_time: datetime, end_time: datetime, lunch_break: int
+    consultant_id: int,
+    customer_id: int,
+    start_time: datetime,
+    end_time: datetime,
+    lunch_break: int,
 ) -> tuple | None:
 
     try:
@@ -133,15 +130,16 @@ def add_time_entry(
             with con.cursor() as cursor:
                 # execute query
                 cursor.execute(
-                    '''
+                    """
                     INSERT INTO time_entries
                     (consultant_id, customer_id, start_time, end_time, lunch_break)
                     VALUES (%s, %s, %s, %s, %s) RETURNING id, consultant_id, customer_id
-                    ''',
+                    """,
                     (consultant_id, customer_id, start_time, end_time, lunch_break),
                 )
                 # fetch (id, consultant_id, customer_id) - return tuple
                 time_created = cursor.fetchone()
                 return time_created
     except (Exception, psycopg2.DatabaseError) as e:
-        print('Database error: ', e)
+        print("Database error: ", e)
+
